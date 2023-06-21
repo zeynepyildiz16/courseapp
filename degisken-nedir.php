7e6c9b61d2077b647fd946be9c940c031ae31163
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-06-21 10:31:58 +0300

    commit

---
+++ degisken-nedir.php
@@ -0,0 +1,17 @@
+<?php
+
+
+
+$maasAli = 19900;
+$maasAren = 20000;
+$maasAras = 21000;
+
+
+$vergiorani1 =0.27;
+$vergiorani2 = 0.35;
+
+echo $maasAli - ($maasAli * $vergiorani2); //Ali maaş
+echo "<br>";
+echo $maasAren - ($maasAren * $vergiorani2); //Aren maaş
+echo "<br>";
+echo $maasAras - ($maasAras * $vergiorani1); //Aras maaş 
