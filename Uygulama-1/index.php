5ece865497aae4f248af3616b4a272ee214b371f
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-06-21 16:15:16 +0300

    Update index.php

---
+++ Uygulama-1/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" />
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container my-3">
+        <div class="card mb-3>">
+            <div class="row">
+                <div class="col-3">
+                    <img src="img/1.jpg" alt="" class="img-fluid rounded-start">
+                </div>
+
+                <div class="col-9">
+                    <div class="card-body">
+                        <h5 class="card-title">Php Kursu</h5>
+                        <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur vel libero reprehenderit quibusdam assumenda laborum rerum corporis eligendi commodi quia!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+</body>
+
+</html>
