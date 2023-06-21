29f3da3392bf5b3249cc8ee9859d9f3fa3b74c6e
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-06-21 14:45:11 +0300

    Update veri-tipleri.php

---
+++ veri-tipleri.php
@@ -0,0 +1,28 @@
+echo "<br>";
+
+$kdvorani = 0.18;
+echo $kdvorani . ' : ' . gettype($kdvorani);
+
+echo "<br>";
+
+$satistaMi = false;
+echo gettype($satistaMi);
+
+echo "<br>";
+
+// Bir değişkenin Türünü Aşağıdaki Yöntemle Değiştirebiliriz.
+
+$a = (float)20;
+echo gettype($a);
+
+echo "<br>";
+
+
+// Burda bir tam sayı değişken türünü ondalıklı değişken türüne döndürdük
+
+
+
+
+
+
+?>
