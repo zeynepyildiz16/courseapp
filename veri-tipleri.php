cbbee9e4d77e3a2ce5099498377996893c89d823
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-06-21 14:57:08 +0300

    Update veri-tipleri.php

--- veri-tipleri.php
+++ veri-tipleri.php
@@ -1,3 +1,26 @@
+<!-- 
+    Php Veri Tipleri
+
+    string   : Metinsel bilgiler
+    int      : tam sayılar
+    double /float  : ondalıklı sayılar 
+    boolean  : true / false bilgi 
+    object   : nesne
+    array    : dizi
+    null     : değer içermeyen bilgi 
+
+ -->
+
+<?php
+
+$urunadi = "Iphone 15";
+echo $urunadi . ' : ' . gettype($urunadi);
+
+echo "<br>";
+
+$fiyat = 75000;
+echo $fiyat . ' : ' . gettype($fiyat);
+
 echo "<br>";
 
 $kdvorani = 0.18;
@@ -14,14 +37,29 @@ echo "<br>";
 
 $a = (float)20;
 echo gettype($a);
-
-echo "<br>";
-
+echo $a . '.' . gettype($a);
 
 // Burda bir tam sayı değişken türünü ondalıklı değişken türüne döndürdük
 
+echo "<br>";
 
+$a = (int)20.5;
+echo $a . '.' . gettype($a);
 
+// Burda bir ondalıklı sayı değişken türünü tamsayı değişken türüne döndürdük.
+
+echo "<br>";
+
+$a = (int)"20";
+echo $a . '.' . gettype($a);
+
+// Burda 20'nin sayı olduğunu tırnak kullanılmasına rağmen anlar çünkü int diyebelirttik.
+echo "<br>";
+
+$a = "20";
+echo $a . '.' . gettype($a);
+
+// Burda 20'nin sayı olduğunu tırnak kullanılması sebebiye anlamaz ve string ifade olarak algılar.
 
 
 
