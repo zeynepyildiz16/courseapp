955d2cd966d9b1b0e7eb08b3f936423b0e1cde66
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-06-21 16:34:25 +0300

    Update index.php

--- Uygulama-1/index.php
+++ Uygulama-1/index.php
@@ -1,3 +1,35 @@
+<?php
+
+
+$kurs1_baslik = "Php Kursu";
+$kurs1_altBaslik = "Sıfırdan ileri seviye Php ile web geliştirme";
+$kurs1_resim = "1.jpg";
+$kurs1_yayinTarihi = "01.01.2023";
+$kurs1_yorumSayisi = "100";
+$kurs1_begeniSayisi = "300";
+
+
+$kurs2_baslik = "Python Kursu";
+$kurs2_altBaslik = "Sıfırdan ileri seviye Python prrogramlama";
+$kurs2_resim = "2.jpg";
+$kurs2_yayinTarihi = "03.03.2023";
+$kurs2_yorumSayisi = "200";
+$kurs2_begeniSayisi = "400";
+
+
+$kurs3_baslik = "Node.js Kursu";
+$kurs3_altBaslik = "Sıfırdan ileri seviye Node.js ile web geliştirme";
+$kurs3_resim = "3.jpg";
+$kurs3_yayinTarihi = "05.05.2023";
+$kurs3_yorumSayisi = "300";
+$kurs3_begeniSayisi = "500";
+
+
+?>
+
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -10,6 +42,34 @@
 
 <body>
     <div class="container my-3">
+        <div class="card mb-3>">
+            <div class="row">
+                <div class="col-3">
+                    <img src="img/1.jpg" alt="" class="img-fluid rounded-start">
+                </div>
+
+                <div class="col-9">
+                    <div class="card-body">
+                        <h5 class="card-title">Php Kursu</h5>
+                        <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur vel libero reprehenderit quibusdam assumenda laborum rerum corporis eligendi commodi quia!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card mb-3>">
+            <div class="row">
+                <div class="col-3">
+                    <img src="img/1.jpg" alt="" class="img-fluid rounded-start">
+                </div>
+
+                <div class="col-9">
+                    <div class="card-body">
+                        <h5 class="card-title">Php Kursu</h5>
+                        <p class="card-text">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consequatur vel libero reprehenderit quibusdam assumenda laborum rerum corporis eligendi commodi quia!</p>
+                    </div>
+                </div>
+            </div>
+        </div>
         <div class="card mb-3>">
             <div class="row">
                 <div class="col-3">
