06f49fd3acedefd80650f1255a593be8df4dda7c
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-06-21 11:33:44 +0300

    Update degisken-tanimlama.php

--- degisken-tanimlama.php
+++ degisken-tanimlama.php
@@ -3,16 +3,20 @@ $ad = "Zeynep";
 $soyad = "YILDIZ";
 
 
-echo $ad." ".$soyad;
+echo $ad . " " . $soyad;
 
+echo "<br>";
 echo "<br>";
 
 $sayi1 = 50;
 $sayi2 = 100;
 
-echo $sayi1." ".$sayi2;
+echo $sayi1 . " " . $sayi2;
+ 
+//Değişken İsminde Boşluk Karakteri Olmaz.
+//Değişken sayi ile başlayamaz.
+
+// Kontrol K + c İle yorum satırı açabilirsiniz . Ya da kontrol K + U ile kapatabilirsin
 
 
-
-
-?>
+// Büyük küçük harf duyarlılığı vardır.
