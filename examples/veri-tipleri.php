222b30f78f0e3590d5718816d8d5e0cbad8cf8d7
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-06-21 15:58:53 +0300

    D

--- examples/veri-tipleri.php
+++ examples/veri-tipleri.php
@@ -36,30 +36,76 @@ echo "<br>";
 // Bir değişkenin Türünü Aşağıdaki Yöntemle Değiştirebiliriz.
 
 $a = (float)20;
-echo gettype($a);
-echo $a . '.' . gettype($a);
+echo $a . " "  . gettype($a);
 
 // Burda bir tam sayı değişken türünü ondalıklı değişken türüne döndürdük
 
 echo "<br>";
 
 $a = (int)20.5;
-echo $a . '.' . gettype($a);
+echo $a . " "  . gettype($a);
 
 // Burda bir ondalıklı sayı değişken türünü tamsayı değişken türüne döndürdük.
 
 echo "<br>";
 
 $a = (int)"20";
-echo $a . '.' . gettype($a);
+echo $a . " "  . gettype($a);
 
 // Burda 20'nin sayı olduğunu tırnak kullanılmasına rağmen anlar çünkü int diyebelirttik.
 echo "<br>";
 
 $a = "20";
-echo $a . '.' . gettype($a);
+echo $a . " "  . gettype($a);
 
 // Burda 20'nin sayı olduğunu tırnak kullanılması sebebiye anlamaz ve string ifade olarak algılar.
+
+echo "<br>";
+
+$a = (int)"a20";
+echo $a . " "  . gettype($a);
+
+/*
+Burda başında a olması onu int olarak algılamasına engel olmaz ancak 
+Rakamsal ifadeyi döndüremediği için sıfır basar.
+*/
+
+echo "<br>";
+
+$a = (int)"20a52";
+echo $a . " "  . gettype($a);
+
+// Burda da çevirebildiği kısmını çevirir geri kalanını gözardı eder.
+
+
+echo "<br>";
+
+$a = true;
+echo $a . " "  . gettype($a);
+
+// Burda true nun karşılığı olarak direkt 1 dğerini gösterir
+
+echo "<br>";
+
+$a = false;
+echo $a . " " . gettype($a);
+
+//Ancak falseda bir şey göstermez.
+
+echo "<br>";
+
+$a = (int)true;
+echo $a . " "  . gettype($a);
+
+// Burda true nun int karşılığı da gelecektir.
+
+echo "<br>";
+
+$a = (int)false;
+echo $a . " "  . gettype($a);
+
+// Burda false un int karşılığı da gelecektir.
+
 
 
 
