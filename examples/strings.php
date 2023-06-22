4642f455e36a5e94744ab950d6c56597c9072884
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-06-22 10:49:19 +0300

    Update strings.php

--- examples/strings.php
+++ examples/strings.php
@@ -6,7 +6,12 @@ $soyad = "AKINCI";
 $yas= 7;
 
 
-$mesaj="Benim adım ".$ad.' '.$soyad." ve ".$yas." yaşındayım.";
+// $mesaj="Benim adım ".$ad.' '.$soyad." ve ".$yas." yaşındayım.";
 
-echo $mesaj;
+$mesaj ="Benim adım {$ad} {$soyad} ve {$yas} yaşındayım.";
+
+
+echo $mesaj."<br>";
+echo $mesaj[0]."<br>"; //M bu dizinin 0. elemanıdır.
+
 ?>
