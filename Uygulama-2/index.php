194d5ce8cc8398eb46368911a1485fe7816486c2
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-06-23 11:15:26 +0300

    Y

--- Uygulama-2/index.php
+++ Uygulama-2/index.php
@@ -2,7 +2,7 @@
 
 
 $kurs1_baslik = "Php Kursu";
-$kurs1_altBaslik = "sıfırdan ileri seviye Php ile web geliştirme";
+$kurs1_altBaslik = "Sıfırdan ileri seviye Php ile web geliştirme";
 $kurs1_resim = "1.jpg";
 $kurs1_yayinTarihi = "01.01.2023";
 $kurs1_yorumSayisi = "100";
@@ -10,7 +10,7 @@ $kurs1_begeniSayisi = "300";
 
 
 $kurs2_baslik = "Python Kursu";
-$kurs2_altBaslik = "sıfırdan ileri seviye Python prrogramlama";
+$kurs2_altBaslik = "Sıfırdan ileri seviye Python prrogramlama";
 $kurs2_resim = "2.jpg";
 $kurs2_yayinTarihi = "03.03.2023";
 $kurs2_yorumSayisi = "200";
@@ -18,11 +18,16 @@ $kurs2_begeniSayisi = "400";
 
 
 $kurs3_baslik = "Node.js Kursu";
-$kurs3_altBaslik = "sıfırdan ileri seviye Node.js ile web geliştirme";
+$kurs3_altBaslik = "Sıfırdan ileri seviye Node.js ile web geliştirme";
 $kurs3_resim = "3.jpg";
 $kurs3_yayinTarihi = "05.05.2023";
 $kurs3_yorumSayisi = "300";
 $kurs3_begeniSayisi = "500";
+
+$kurs1_altBaslik = ucfirst(strtolower($kurs1_altBaslik));
+$kurs2_altBaslik = ucfirst(strtolower($kurs2_altBaslik));
+$kurs3_altBaslik = ucfirst(strtolower($kurs3_altBaslik));
+
 
 
 ?>
@@ -47,7 +52,6 @@ $kurs3_begeniSayisi = "500";
                 <div class="col-3">
                     <img src="img/<?php echo $kurs1_resim; ?>" alt="" class="img-fluid rounded-start">
                 </div>
-
                 <div class="col-9">
                     <div class="card-body">
                         <h5 class="card-title"><?php echo $kurs1_baslik; ?></h5>
