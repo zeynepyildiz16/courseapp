24692226ab2a38c3cd826306ff1c462d1d26bbb7
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-06-23 10:58:14 +0300

    Create sabitler.php

---
+++ examples/sabitler.php
@@ -0,0 +1,20 @@
+<?php
+// PHP Sabitler:
+// Sabitler veri saklamak için kullanılır ancak tanımlanan değer değiştirilmez.
+
+
+const baglanti = "Mysql bağlantı cümlesi.";   
+//şeklinde de sabiti tanımlaya biliriz.
+
+echo baglanti;
+//define("baglanti","Mysql bağlantı cümlesi.");
+// Define bir sabit tanımlamak için kullanılır. 
+// Const baglanti = "Mysql bağlantı cümlesi.";   şeklinde de sabiti tanımlaya biliriz.
+// Tanımlanan sabit değişkenlerin adındanda anlaşılacağı üzere değerleri değiştirilemez.
+
+const username = "zeynepyildiz";
+const password  = "1234";
+
+
+
+?>
