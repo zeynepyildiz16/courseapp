068a39bfe947c5e4348d6738853408af1ed3a01c
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-08-07 11:44:26 +0300

    Create dizi-fonksiyonlari.php

---
+++ examples/dizi-fonksiyonlari.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    // Array Functions
+
+    $notlar = array(60, 70, 40, 80);
+    echo count($notlar);
+    // Dizi elemanlarının sayısını count (say) ile bulduk. 
+
+
+    $plakalar = array("41" => "kocaeli", "34" => "istanbul", "35" => "izmir");
+    echo "<br>";
+    echo count($plakalar);
+
+
+    
+    // Diziye yeni bir eleman eklemek istediğimiz zaman
+
+
+
+
+    ?>
+</body>
+
+</html>
