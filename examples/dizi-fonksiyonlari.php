b878f713fa93b43e75dc4ab58d6a3282440008db
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-08-07 13:26:00 +0300

    Update dizi-fonksiyonlari.php

--- examples/dizi-fonksiyonlari.php
+++ examples/dizi-fonksiyonlari.php
@@ -13,17 +13,25 @@
     // Array Functions
 
     $notlar = array(60, 70, 40, 80);
-    echo count($notlar);
-    // Dizi elemanlarının sayısını count (say) ile bulduk. 
+
+
 
 
     $plakalar = array("41" => "kocaeli", "34" => "istanbul", "35" => "izmir");
     echo "<br>";
-    echo count($plakalar);
 
 
-    
-    // Diziye yeni bir eleman eklemek istediğimiz zaman
+
+    // Dizi elemanlarının sayısını count (say) ile bulduk. 
+    // Diziye yeni bir eleman eklemek istediğimiz zaman array_push fonksiyonunu kullandık
+
+    array_push($notlar, 100);
+    echo count($notlar) . "<br>";
+    echo count($plakalar) . "<br>";
+
+    print_r($notlar) . "<br>";
+    echo "<br>";
+    print_r($plakalar) . "<br>";
 
 
 
