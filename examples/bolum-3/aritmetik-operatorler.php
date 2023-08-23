462b0da87daa2322765730ed8ac4a1b420339783
zeynepyildiz16 <zeynepyildizbursa@gmail.com>
2023-08-23 21:43:07 +0300

    Update aritmetik-operatorler.php

---
+++ examples/bolum-3/aritmetik-operatorler.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    /* 
+    
+        +        Toplama     $x + $y
+        -        Çıkarma     $x - $y
+        *        Çarpma      $x * $y
+        /        Bölme       $x / $y
+        %        Mod Alma    $x % $y
+        **       Üs Alma     $x ** $y
+        ++$x     İşlem Öncesi Arttırma 
+        --$x     İşlem Öncesi Azaltma
+        $x++     İşlem Sonrası Arttırma
+        $x--     İşlem Sonrası Azaltma
+    */
+
+    $a = 10;
+    $b = 20;
+
+    echo $a + $b ;
+
+
+    ?>
+</body>
+</html>
